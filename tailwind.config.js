9b3ef25c65bb9d921594eb5225a55219972c03a3
mdmunnaahmed <msdmunna77@gmail.com>
2024-10-14 04:39:59 +0600

    config --update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*", "./dist/**/*.html"],
+  content: ["*", "./dist/*.html"],
   theme: {
     screens: {
       sm: "576px",
@@ -13,27 +13,27 @@ module.exports = {
       center: true,
       padding: "1rem",
     },
-    fontFamily: {
-      heading: ['"Exo", sans-serif;'],
-      body: ['"Public Sans", sans-serif'],
-    },
-    fontSize: {
-      sm: "0.875rem",
-      md: "0.875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "3rem",
-      "4xl": "4rem",
-    },
-    boxShadow: {
-      small: "0px 4px 30px rgba(1, 19, 60, 0.08);",
-      base: "0px 6px 30px rgba(7, 36, 95, 0.08)",
-      large: "0px 6px 50px 0px rgba(0, 0, 0, 0.06);",
-    },
 
     extend: {
+      fontFamily: {
+        heading: ['"Exo", sans-serif;'],
+        body: ['"Public Sans", sans-serif'],
+      },
+      fontSize: {
+        sm: "0.875rem",
+        md: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "3rem",
+        "4xl": "4rem",
+      },
+      boxShadow: {
+        small: "0px 4px 30px rgba(1, 19, 60, 0.08);",
+        base: "0px 6px 30px rgba(7, 36, 95, 0.08)",
+        large: "0px 6px 50px 0px rgba(0, 0, 0, 0.06);",
+      },
       colors: {
         primary: "#FF5E14",
         title: "#0E121D",
